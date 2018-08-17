32223d1bd1ed2d29b396e88ab6204c3bd84aee7c
Christian Wolf <c.wolf@lsr.uni-saarland.de>
2018-08-17 11:50:50 +0200

    Added template in installation

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ RDIR=tex/latex/Stundenzettel
 
 install -vd "$TEXMF/$RDIR"
 install -v -m 644 stundenzettel.cls "$TEXMF/$RDIR"
+install -v -m 644 stundenzettel.tex "$TEXMF/$RDIR/stundenzettel.tex.templ"
 
 mktexlsr "$TEXMF"
 
